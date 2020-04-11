800b4072546ae59c67c58582f9901ad9a78b3550
Yogi Nugraha <yoginugraha43@gmail.com>
2020-04-11 10:39:28 +0700

    commit pertama

---
+++ app/src/main/java/id/ac/unpas/booksearch/FetchBook.java
@@ -0,0 +1,53 @@
+package id.ac.unpas.booksearch;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class FetchBook extends AsyncTask<String, Void, String> {
+    private TextView mTitleText, mAuthorText;
+
+    public FetchBook(TextView mTitleText, TextView mAuthorText) {
+        this.mTitleText = mTitleText;
+        this.mAuthorText = mAuthorText;
+    }
+    @Override
+    protected String doInBackground(String... strings) {
+        return NetworkUntils.getBookInfo(strings[0]);
+    }
+    @Override
+    protected void onPostExecute(String s){
+        super.onPostExecute(s);
+        try {
+            JSONObject jsonObject = new JSONObject(s);
+            JSONArray jsonArray = jsonObject.getJSONArray("items");
+
+            for (int i = 0; i < itemsArray.length(); i++) {
+                JSONObject book = itemsArray.getJSONObject(i);
+                String title = null;
+                String authors = null;
+                JSONObject volumeInfo = book.getJSONObject("vomuleInfo");
+
+                try{
+                    title = volumeInfo.getString("title");
+                    authors = volumeInfo.getString("authors");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                if (title != null && authors != null) {
+                    mTitleText.setText(title);
+                    mAuthorText.setText(authors);
+                    return;
+                }
+            }
+            mTitleText.setText("No Reslut Found");
+            mAuthorText.setText("");
+        }catch (Exception e){
+            mTitleText.setText("No Result Found");
+            mAuthorText.setText("");
+            e.printStackTrace();
+        }
+    }
+}
